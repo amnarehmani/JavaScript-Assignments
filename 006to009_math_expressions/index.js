92a7358ba39f0eee0a57ec999e55612d21444025
amnarehmani <jeonamnaaa@gmail.com>
2024-05-06 17:01:26 +0500

    JavaScript Assignments

---
+++ 006to009_math_expressions/index.js
@@ -0,0 +1,88 @@
+// Question no 01
+// var number = 10;
+
+// document.write("<h1>Result</h1>");
+// document.write("The Value of number is: " , number);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("The Value of ++a is: ", ++number);
+// document.write("<br>");
+// var number2 = 11;
+// document.write("Now Value of number is: " , number2);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("The Value of a++ is: ", number++);
+// document.write("<br>");
+
+// var number3 = 12
+
+// document.write("Now Value of number is: " , number3);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("The Value of --a is: ", --number3);
+// document.write("<br>");
+
+// document.write("Now Value of number is: " , -number2);
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("The Value of a-- is: ", number3--);
+// document.write("<br>");
+// document.write("Now the value of number is: ", number3--)
+
+
+
+
+// Question no 02
+// var a = 2
+// var b = 1
+// document.write (--a );
+// document.write("<br>");
+// document.write(--a - --b);
+// document.write("<br>");
+// document.write(--a - --b + ++b);
+// document.write("<br>");
+// document.write(--a - --b+ ++b + b--);
+
+
+// Question no 03
+// prompt( "Hello! Whats your name?")
+
+
+// Question no 05
+
+// var input = +prompt("Enter a number" , 5);
+// var input2 = +prompt("Enter 2nd number" , 5)
+
+// document.write(input*input2)
+
+
+
+// Question no 06
+
+var sub1 = +prompt ("ENTER SUBJECT ONE NAME");
+
+var sub2 = +prompt ("ENTER SUBJECT TWO NAME");
+
+var sub3 = +prompt ("ENTER SUBJECT THREE NAME");
+
+var sub1Totalmarks = 100;
+
+var sub2Totalmarks = 100;
+
+var sub3Totalmarks = 100;
+
+var obtained1 = +prompt ("ENTER MARKS");
+
+var obtained2 = +prompt ("ENTER MARKS");
+
+var obtained3 = +prompt ("ENTER MARKS");
+
+
+
+
+ 
+
+
+
+  
