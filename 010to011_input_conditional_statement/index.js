d021d3b47fb83cec4bb7feb172e3a6e6a8d12e09
amnarehmani <jeonamnaaa@gmail.com>
2024-05-13 22:21:15 +0500

    JavaScript Assignments SMIT

---
+++ 010to011_input_conditional_statement/index.js
@@ -0,0 +1,159 @@
+// Question no: 01
+//  var city = prompt("ENTER CITY NAME");
+//  if(city === "Karachi"){
+//     document.write("City of lights")
+//  }
+
+// Question no: 02
+
+// var gender = prompt("Your gender?");
+// if(gender === "Male"){
+//     document.write("Good Morning SIR")
+// }
+// if(gender === "Female"){
+//     document.write("Good Morning Ma'am")
+// }
+
+
+// Question no: 03
+
+// var signals = prompt("Enter traffic signals light colors");
+// if(signals === "Red"){
+//     document.write("Must stop");
+// };
+// if(signals === "Yellow"){
+//     document.write("Ready to move");
+// };
+// if(signals === "Green"){
+//     document.write("Move now");
+// };
+
+
+// Question no: 04
+
+// var fuel = prompt("Enter your remaining fuel in car")
+// if(fuel == 0.25){
+//     document.write("Please refill the fuel in car")
+// }
+
+// Question no: 05
+// Variable A
+// var a = 4;
+// if(++a === 5){
+//     alert("Given condition in variable A is true")
+// }
+// Variable B
+// var b = 82;
+// if(b++ === 83){
+//     alert("Given condition in variable B true")
+// }
+// Variable C
+
+// var c = 12;
+// if(c++ === 13){
+//     alert("Given condition in variable C true")
+// }
+
+// if(++c < 14){
+//     alert("Given condition in variable D true")
+// }
+// if(++c < 14){
+//     alert("Given condition in variable D true")
+// }
+
+// Variables D
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// Variables E
+
+// if (true){
+//     alert("True");
+//     }
+// if (false){
+//     alert("False");
+//     }
+
+
+
+// Question no: 06
+// document.write("<h1>Marks Sheet</h1>")
+
+// var marks = prompt ("Enter your total marks")
+// if(marks<= 80){
+//     document.write("<h3>Grade</h3>A-one <h3>Remakrs</h3>Excellent")
+// }
+
+// if(marks <= 70){
+//     document.write("<h3>Grade</h3>A<h3>Remakrs</h3>Good")
+// }
+// if(marks <= 60){
+//     document.write("<h3>Grade</h3>B<h3>Remakrs</h3>You need to improve")
+// }
+// if(marks >= 60){
+//     document.write("<h3>Grade</h3>fail<h3>Remakrs</h3>sorry")
+// }
+
+    // Question  no: 07
+
+    // var game = prompt("Guess a number from 1 to 10");
+    // if(game == 7){
+    //     document.write("Bingo! you guess right number")
+    // }
+   
+
+    // Question no: 08
+    // var program = +prompt("Enter number that is divisible by 3");
+    // var num= 12;
+    // if(num/3){
+    //        document.write("Divisible by 3")
+    // }
+    // else{
+    //     document.write("Nah!")
+    // }
+
+
+    // Question no: 09
+    // var Num = +prompt("Write number");
+    // var num1 = 4;
+    // if(num1==4){
+    //     document.write("EVEN NUMBER")
+    // }else{
+    //     document.write("ODD NUMBER")
+    // }
+   
+
+    // Question no: 10
+
+    // var tem = +prompt("today's temperature?");
+    // if(tem > 40){
+    
+    //     console.log("“It is too hot outside.”")
+    // }
+    // if(tem > 30){
+    
+    //     console.log("“The Weather today is Normal.”")
+    // }
+    // if(tem > 20){
+    
+    //     console.log("“Today’s Weather is cool.”")
+    // }
+    // if(tem > 10){
+    
+    //     console.log("“OMG! Today’s weather is so Cool.”")
+    // }
+    
+
+    // Question no: 11
+    // var num1 = +prompt("Enter number");
+    // var num2 = +prompt("Enter Second number");
+
+    // console.log(num1 + num2);
+    // console.log(num1 - num2);
+    // console.log(num1 * num2);
+    // console.log(num1 / num2);
+    // console.log(num1 % num2);
