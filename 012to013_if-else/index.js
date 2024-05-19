ddff4ec12d3d1a671f351ef57a90b7af4f8b3555
amnarehmani <jeonamnaaa@gmail.com>
2024-05-20 01:33:51 +0500

    JavaScript Assignment

---
+++ 012to013_if-else/index.js
@@ -0,0 +1,166 @@
+// Question no 01
+
+// let answers = prompt("Enter Your Value")
+// let value1 = "A";
+// let value2 = "Z";
+// let value3 = "z";
+// let value4 = "a";
+
+// if(answers == value1){
+//     console.log(`${value1} = 65`)
+// }
+// else if(answers == value2){
+//     console.log(`${value2} = 90`)
+// }
+// else if(answers == value3){
+//     console.log(`${value3} = 122`)
+// }
+// else if(answers == value4){
+//     console.log(`${value4} = 97`)
+// }
+// else{
+//     console.log("wrong value")
+// }
+
+
+// Question no 02
+
+// let int1 = +prompt("Enter Your First Integer");
+// let int2 = +prompt("Enter Your Second Integer");
+
+// if(int1 > int2){
+//     console.log(`${int1} is larger than ${int2}`)
+// }
+// else if(int1 < int2){
+//     console.log(`${int2} is larger than ${int1}`)
+// }
+// else{
+//     console.log("Both integers are equal")
+// }
+
+// Question no 03
+
+// let numbers = +prompt("Enter your number")
+// if(numbers > 0){
+//     console.log(`${numbers} is a positive number`)
+// }
+// else if (numbers < 0){
+//     console.log(`${numbers} is a negative number`)
+// }
+// else{
+//     console.log("The number is 0")
+// }
+
+
+// Question no 04
+
+// let characters = prompt("Enter your character");
+// let letters1 = "a";
+// let letters2 = "e";
+// let letters3 = "i";
+// let letters4 = "o";
+// let letters5 = "u";
+
+// if(characters == letters1){
+//     console.log(`${letters1} this character is a vowel`)
+// }
+// else if(characters == letters1){
+//     console.log(`${letters1} this character is a vowel`)
+// }
+// else if(characters == letters2){
+//     console.log(`${letters2} this character is a vowel`)
+// }
+// else if(characters == letters3){
+//     console.log(`${letters3} this character is a vowel`)
+// }
+// else if(characters == letters4){
+//     console.log(`${letters4} this character is a vowel`)
+// }
+// else if(characters == letters5){
+//     console.log(`${letters5} this character is a vowel`)
+// }
+// else{
+//     console.log("false")
+// }
+
+
+
+// Question  no 05 
+
+// let answers = prompt("Enter your Password")
+
+// let password = "ABC123";
+
+// if(answers == password){
+//     alert("Your Password is correct!")
+// }
+// else{
+//     alert("Incorrect password!")
+// }
+
+
+// Question no 06
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// console.log("Good day");
+// }
+// else{
+// console.log("Good evening");
+// }
+
+
+// Question no 07
+
+// let timing = prompt("What time is it?")
+
+// let = time1 = "1pm";
+// let = time2 = "2pm";
+// let = time3 = "3pm";
+// let = time4 = "4pm";
+// let = time5 = "5pm";
+// let = time6 = "6pm";
+// let = time7 = "7pm";
+// let = time8 = "8pm";
+// let = time9 = "9pm";
+// let = time10 = "10pm";
+// let = time11 = "11pm";
+// let = time12 = "12am";
+
+// if(timing == time1){
+//     console.log(`${time1} = 13:00`)
+// }
+// else if(timing == time2){
+//     console.log(`${time2} = 14:00`)
+// }
+// else if(timing == time3){
+//     console.log(`${time3} = 15:00`)
+// }
+// else if(timing == time4){
+//     console.log(`${time4} = 16:00`)
+// }
+// else if(timing == time5){
+//     console.log(`${time5} = 17:00`)
+// }
+// else if(timing == time6){
+//     console.log(`${time6} = 18:00`)
+// }
+// else if(timing == time7){
+//     console.log(`${time7} = 19:00`)
+// }
+// else if(timing == time8){
+//     console.log(`${time8} = 20:00`)
+// }
+// else if(timing == time9){
+//     console.log(`${time9} = 21:00`)
+// }
+// else if(timing == time10){
+//     console.log(`${time10} = 22:00`)
+// }
+// else if(timing == time11){
+//     console.log(`${time11} = 23:00`)
+// }
+// else if(timing == time12){
+//     console.log(`${time12} = 24:00`)
+// }
