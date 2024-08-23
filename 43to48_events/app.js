66df8f6b5c2bd88bef313889222851f9a12f72cd
amnarehmani <jeonamnaaa@gmail.com>
2024-08-23 20:48:29 +0500

    javascript assignment

--- 43to48_events/app.js
+++ 43to48_events/app.js
@@ -1,1 +1,18 @@
-let alertMessage = document.querySelector("img")
+// function deleteRow(button){
+//     let row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+// }
+
+// let counter = 0
+
+// function increase() {
+
+//    document.getElementById("counter").innerText = counter++
+// }
+// function decrease() {
+
+//    if(counter < 0){
+//       counter = 0
+//    }
+//    document.getElementById("counter").innerText = counter--
+// }
